0ef72b3166bff3ddee69954fb134a82647d79646
Dominik Špoljarić <d.spoljaric@student.tudelft.nl>
2023-12-14 18:05:39 +0100

    nothing

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import numpy
+import matplotlib.pyplot as plt
 
-#this is sample code
-# hello
+# =============================== Simulation Constants ===================================
+thrust = 2000 # [N]
+r_thrust = 0.85 # [-], location of force as a fraction of propeller radius
+
+
+
+
+
+if __name__ == "__main__":
+    print("hi")
