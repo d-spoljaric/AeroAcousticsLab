abc103c8ad49f8261311e2f2eab79e1a6098211c
Dominik Špoljarić <d.spoljaric@student.tudelft.nl>
2024-01-11 15:59:33 +0100

    Whole buncha shit pt.5

--- frequency_approach.py
+++ frequency_approach.py
@@ -17,23 +17,24 @@ M_tip = 0.3  # [-], propeller tip Mach number
 M_force = M_tip * r_thrust  # [-], Mach number at force location
 D = 1  # [m], propeller radius
 R_1 = r_thrust * D / 2  # [m], magnitude of location of force application vector
-B = 1  # [-], number of propeller blades
+B = 4  # [-], number of propeller blades
 c = np.sqrt(1.4 * 287.15 * T)  # [m/s], speed of sound
 vel_rad = M_tip * r_thrust * c / R_1  # [rad/s], radial velocity of the location of force application
 blade_offset = 2 * np.pi / B  # [rad], constant angle offset for each blade of the propeller
 
 # ============================== Chosen Simulation Constants ==================================
 R_0 = 100  # [m], magnitude of observer location vector
-observer_phi = np.pi / 4  # [rad], phi angle of observer position
-# observer_theta_list = np.arange(0., 105, 15)
-observer_theta_list = np.linspace(0.001, 90, 100)
-# observer_theta_list = np.array([0])
+observer_phi = 45  # [deg], phi angle of observer position
+# observer_theta_list = np.arange(0.0001, 105, 15)
+observer_theta_list = np.linspace(0.00001, 90, 50)
+# observer_theta_list = np.array([45])
 
-periodic_force = 100 #[N], amplitude of periodic force
-n = 1 # Determines the harmonic of the force
+periodic_force = 1000  # [N], amplitude of periodic force
+n = 1  # Determines the harmonic of the periodic force
 
-m_val = 10
+m_val = 50
 s_val = 0
+
 
 # ============================= Sound Functions ========================================
 def compute_spl(p: np.ndarray) -> int | float:
@@ -57,7 +58,7 @@ def compute_rms(x: np.ndarray) -> int | float:
     return np.sqrt(np.mean(x ** 2))
 
 
-def pressure(phi: float, theta: float, force_func: object, s: np.ndarray | float, m: int) -> float:
+def pressure(phi: float | int, theta: float | int, force_func: object, s: np.ndarray | float, m: int) -> float:
     global B
     global vel_rad
     global R_0
@@ -65,57 +66,53 @@ def pressure(phi: float, theta: float, force_func: object, s: np.ndarray | float
     global thrust_total
     global M_force
 
-    theta = np.deg2rad(theta)
+    theta = np.radians(theta)
+    phi = np.radians(phi)
 
-    if s == 0:
-        coeff = 1j * m * (B ** 2) * vel_rad / (4 * np.pi * c * R_0)
-        exp_term = np.exp(-1j * m * B * np.pi / 2) * np.exp(1j * m * B * (phi - vel_rad * R_0 / c))
-        Fs = thrust_total/B
-        bessel = sp.special.jv(m * B, m * B * M_force * np.sin(theta))
-        p_mB = coeff * Fs * exp_term * bessel * np.cos(theta)
-    else:
-        Fs_real = (vel_rad / (2 * np.pi)) * \
-                  sp.integrate.quad(lambda t: force_func(t, s) * np.real(np.exp(1j * s * vel_rad * t)), 0,
-                                    2 * np.pi / vel_rad)[0]
-        Fs_imag = (vel_rad / (2 * np.pi)) * \
-                  sp.integrate.quad(lambda t: force_func(t, s) * np.imag(np.exp(1j * s * vel_rad * t)), 0,
-                                    2 * np.pi / vel_rad)[0]
-        Fs = (Fs_real + 1j * Fs_imag)/B
+    Fs_real = (vel_rad / (2 * np.pi)) * \
+              sp.integrate.quad(lambda t: force_func(t) * np.real(np.exp(1j * s * vel_rad * t)), 0,
+                                2 * np.pi / vel_rad)[0]
+    Fs_imag = (vel_rad / (2 * np.pi)) * \
+              sp.integrate.quad(lambda t: force_func(t) * np.imag(np.exp(1j * s * vel_rad * t)), 0,
+                                2 * np.pi / vel_rad)[0]
+    Fs = (Fs_real + 1j * Fs_imag) / B
 
-        coeff = 1j * m * (B ** 2) * vel_rad * np.exp(-1j * m * B * vel_rad * R_0 / c) / (4 * np.pi * c * R_0)
-        exp_term = np.exp(1j * (m * B - s) * (phi - np.pi / 2))
-        bessel = sp.special.jv(m * B - s, m * B * np.sin(theta))
+    coeff = -1j * m * (B ** 2) * vel_rad * np.exp(-1j * m * B * vel_rad * R_0 / c) / (4 * np.pi * c * R_0)
+    exp_term = np.exp(-1j * (m * B - s) * (phi - np.pi / 2))
+    bessel = sp.special.jv(m * B - s, m * B * M_force * np.sin(theta))
 
-        sigma_term = np.sum(Fs * exp_term * bessel * np.cos(theta))
-        p_mB = coeff * sigma_term
+    sigma_term = np.sum(Fs * exp_term * bessel * np.cos(theta))
+    p_mB = coeff * sigma_term
 
     return p_mB
 
 
-def force(t, s):
+def force_periodic(t):
     global thrust_total
     global periodic_force
     global n
-    if s == 0:
-        return thrust_total
-    else:
-        period = (1 / n) * 2 * np.pi / vel_rad
-        A = 2 * np.pi / period  # Do not edit this
-        return thrust_total + periodic_force * np.cos(A * t)
+    period = (1 / n) * 2 * np.pi / vel_rad
+    A = 2 * np.pi / period  # Do not edit this
+    return thrust_total + periodic_force * np.cos(A * t)
 
+
+def force_constant(t):
+    global thrust_total
+    return thrust_total
 
 
 # ============================== Main Loop ===============================================
 
+constant_force = True
+
 # Simulation arrays
-m_array = np.arange(-m_val, m_val+1,1)
-s_array = np.arange(-s_val, s_val+1, 1)
+m_array = np.arange(-m_val, m_val + 1, 1)
+s_array = np.arange(-s_val, s_val + 1, 1)
 
 p_mB_array = np.zeros(shape=(len(observer_theta_list), len(m_array)), dtype=np.complex_)
-p_rms = np.zeros(shape = observer_theta_list.shape)
-SPL_array = np.zeros(shape = observer_theta_list.shape)
-PWL_array = np.zeros(shape = observer_theta_list.shape)
-
+p_rms = np.zeros(shape=observer_theta_list.shape)
+SPL_array = np.zeros(shape=observer_theta_list.shape)
+PWL_array = np.zeros(shape=observer_theta_list.shape)
 
 # Calculating p_mB for each theta and m
 for count_theta in tqdm(range(observer_theta_list.shape[0]), desc="Looping over theta"):
@@ -124,7 +121,10 @@ for count_theta in tqdm(range(observer_theta_list.shape[0]), desc="Looping over 
         m = m_array[count_m]
         p_mB = 0 + 0j
         for s in s_array:
-            p_mB_s = pressure(observer_phi, theta, force, s, m)
+            if constant_force:
+                p_mB_s = pressure(observer_phi, theta, force_constant, s, m)
+            else:
+                p_mB_s = pressure(observer_phi, theta, force_periodic, s, m)
             p_mB += p_mB_s
         p_mB_array[count_theta, count_m] = p_mB
 
@@ -136,6 +136,9 @@ for count_theta in tqdm(range(observer_theta_list.shape[0]), desc="Looping over 
     p_mB_ifft[0] = p_mB_temp[zero_freq_idx]
     p_mB_ifft[1:m_val + 1] = p_mB_temp[zero_freq_idx + 1:]
     p_mB_ifft[m_val + 1:] = p_mB_temp[:zero_freq_idx]
+
+    # plt.scatter(m_array * B, np.real(p_mB_array))
+    # plt.show()
 
     p_mB_array[count_theta, :] = p_mB_ifft
 
@@ -149,4 +152,6 @@ for count_theta in tqdm(range(observer_theta_list.shape[0]), desc="Looping over 
     SPL_array[count_theta] = compute_spl(p_t)
 
 plt.plot(observer_theta_list, SPL_array)
+plt.ylim([0, 60])
+# plt.scatter(m_array*B, p_mB_array)
 plt.show()
--- test.py
+++ test.py
@@ -1,29 +1,35 @@
 import numpy as np
 import scipy as sp
 
-mB = 4
-s = 1
+# mB = 4
+# s = 1
+#
+# coeff = -1j*np.exp(-1j*mB)
+#
+# def force_time(t):
+#     return np.cos(t)
+#
+# def force_s(s):
+#     Fs_real = sp.integrate.quad(lambda t: np.real(force_time(t)*np.exp(1j*s*t)), 0, 1)
+#     Fs_imag = sp.integrate.quad(lambda t: np.imag(force_time(t)*np.exp(1j*s*t)), 0, 1)
+#     return Fs_real[0] + 1j*Fs_imag[0]
+#
+# phi = np.pi/4
+# # Negative s
+# neg_s = force_s(-s)*np.exp(1j*(mB-(-s))*(phi-np.pi/2))
+# # Positive s
+# pos_s = force_s(s)*np.exp(1j*(mB-(s))*(phi-np.pi/2))
+#
+# tot_s = neg_s+pos_s
+#
+# print(neg_s)
+# print(pos_s)
+# print(tot_s)
 
-coeff = -1j*np.exp(-1j*mB)
+# print(coeff*tot_s)
 
-def force_time(t):
-    return np.cos(t)
+a_f = [0, 0.005 + .002j, 0.002 + 0.001j, 0.001 + 0.0001j, 0.001 - 0.0001j, 0.005 - .002j, 0.002 - 0.001j]
+a_t = np.fft.ifft(a_f)
+a_rms = np.square(np.mean(a_t**2))
 
-def force_s(s):
-    Fs_real = sp.integrate.quad(lambda t: np.real(force_time(t)*np.exp(1j*s*t)), 0, 1)
-    Fs_imag = sp.integrate.quad(lambda t: np.imag(force_time(t)*np.exp(1j*s*t)), 0, 1)
-    return Fs_real[0] + 1j*Fs_imag[0]
-
-phi = np.pi/4
-# Negative s
-neg_s = force_s(-s)*np.exp(1j*(mB-(-s))*(phi-np.pi/2))
-# Positive s
-pos_s = force_s(s)*np.exp(1j*(mB-(s))*(phi-np.pi/2))
-
-tot_s = neg_s+pos_s
-
-print(neg_s)
-print(pos_s)
-print(tot_s)
-
-print(coeff*tot_s)
+print(np.real(a_rms))
